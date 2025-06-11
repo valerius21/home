d1e71ec0670b1c689a89e990aed5135bd6433d06
Valerius Mattfeld <git@valerius.me>
2025-06-11 11:32:07 +0200

    add submodule

--- home.nix
+++ home.nix
@@ -49,6 +49,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+		".gitconfig".source = dotfiles/etc/dot-gitconfig;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
