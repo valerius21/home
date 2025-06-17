877bfc1685f26d0c6472a3a18a7f44125f9894e6
Valerius Mattfeld <git@valerius.me>
2025-06-17 19:37:12 +0200

    chore: remove .gitconfig mapping due to dotfiles submodule removal

--- home.nix
+++ home.nix
@@ -49,7 +49,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-		".gitconfig".source = ./dotfiles/etc/dot-gitconfig;
+    # Removed .gitconfig mapping since dotfiles submodule is gone
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
